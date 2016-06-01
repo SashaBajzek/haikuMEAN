db3613a7fb0c1dc43ef25ed5e881ee66b0cec983
Sasha Bajzek <dabridginator@gmail.com>
2016-05-31 21:41:27 -0700

    Update factory test

--- tests/haiku.factory.test.js
+++ tests/haiku.factory.test.js
@@ -1,5 +1,30 @@
-describe('haikuFactory', function () {
-	it('create haiku', function () {
-		//expect().toBe();
+describe('haikuFactory', function (service) {
+	
+	beforeEach(angular.mock.module('haikuForYou'));
+	
+	var $controller;
+	var service;
+	var httpBackend;
+	
+	beforeEach(angular.mock.inject(function(_$controller_){
+		$controller = _$controller_;
+	}));
+	
+	beforeEach(function () {
+		angular.mock.inject(function ($injector) {
+			httpBackend = $injector.get('$httpBackend');
+			service = $injector.get('service');
+		})
 	});
+	
+	describe('getAll', function () {
+		it("should return list of all haikus", inject(function () {
+			httpBackend.expectGET('/haukus').respond(['haiku 1', 'haiku 2', 'haiku 3']);
+			service.getAll(function (result) {
+				expect(result).toEqual(['haiku 1', 'haiku 2', 'haiku 3']);
+			});
+			httpBackend.flush();
+		}))
+	});
+
 });
